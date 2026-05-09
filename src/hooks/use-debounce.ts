4d620778bda66d12d53dbd7f3a5791f10be7db2a
Tugeru <andyzaneegut250@gmail.com>
2026-05-10 02:13:11 +0800

    feat(hooks): add useDebounce hook
    Debounced callback hook for faculty search input.

---
+++ src/hooks/use-debounce.ts
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from "react";
+
+export function useDebounce<T extends (...args: Parameters<T>) => ReturnType<T>>(
+  callback: T,
+  delay: number
+): T {
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  const debouncedCallback = ((...args: Parameters<T>) => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+    timeoutRef.current = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  }) as T;
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
+  return debouncedCallback;
+}
